b49e211d02abae0b742a392b78d2f74ee431d783
Umair Ahmed <umairjdu@gmail.com>
2025-06-17 23:11:11 +0500

    process2

--- netlify/functions/getCountry.js
+++ netlify/functions/getCountry.js
@@ -3,13 +3,8 @@ exports.handler = async function (event) {
   const agent_id = "agent_01jxdnrec8f2x9nk0yxyqgpdzz";
   const ELE_KEY = "sk_ca703d8cad15206b7a5eb88b17a4c0ab57f714050d8eac07";
 
-  console.log({
-    Ele_key: process.env.ELEVENLABS_API_KEY,
-    API_URI: process.env.API_URI,
-  });
-
   try {
-    const apiUrl = `https://api.elevenlabs.io/v1/convai/conversation/get-signed-url?agent_id=${agent_id}`;
+    const apiUrl = `${process.env.API_URI}/v1/convai/conversation/get-signed-url?agent_id=${agent_id}`;
     const response = await fetch(apiUrl, {
       headers: {
         "xi-api-key": ELE_KEY,
