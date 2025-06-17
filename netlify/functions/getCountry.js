084ca99871c0a6b549214c99239ee057846544a2
Umair Ahmed <umairjdu@gmail.com>
2025-06-17 23:16:37 +0500

    same problem

--- netlify/functions/getCountry.js
+++ netlify/functions/getCountry.js
@@ -1,8 +1,7 @@
 // update
 exports.handler = async function (event) {
   const agent_id = "agent_01jxdnrec8f2x9nk0yxyqgpdzz";
-  // const ELE_KEY = "sk_ca703d8cad15206b7a5eb88b17a4c0ab57f714050d8eac07";
-  const ELE_KEY = process.env.ELEVENLABS_API_KEY;
+  const ELE_KEY = "sk_ca703d8cad15206b7a5eb88b17a4c0ab57f714050d8eac07";
 
   try {
     const apiUrl = `${process.env.API_URI}/v1/convai/conversation/get-signed-url?agent_id=${agent_id}`;
