da22c5a8dd7423aff7b885baf58c9c7aa699619f
Umair Ahmed <umairjdu@gmail.com>
2025-06-17 23:08:19 +0500

    process2

--- netlify/functions/getCountry.js
+++ netlify/functions/getCountry.js
@@ -3,7 +3,10 @@ exports.handler = async function (event) {
   const agent_id = "agent_01jxdnrec8f2x9nk0yxyqgpdzz";
   const ELE_KEY = "sk_ca703d8cad15206b7a5eb88b17a4c0ab57f714050d8eac07";
 
-  console.log(ELE_KEY);
+  console.log({
+    Ele_key: process.env.ELEVENLABS_API_KEY,
+    API_URI: process.env.API_URI,
+  });
 
   try {
     const apiUrl = `https://api.elevenlabs.io/v1/convai/conversation/get-signed-url?agent_id=${agent_id}`;
